a9b58bf884ad66ce6639987ff6260f2c65b83f7d
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-19 21:18:27 +0530

    chnages

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+    import { useNavigate, useParams } from 'react-router-dom';
 // import { useSelector } from 'react-redux';
 import { useDispatch, useSelector } from "react-redux";
 import { updateUser } from '../redux/UserReducer';
